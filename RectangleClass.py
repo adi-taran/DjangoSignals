cf6da2a050abbd7022f686158e5415f0f5543048
Aditya Jain <119096332+adi-taran@users.noreply.github.com>
2025-02-14 16:42:29 +0530

    Update RectangleClass.py

--- RectangleClass.py
+++ RectangleClass.py
@@ -1,24 +1,22 @@
-class Rectangle: #defined class with the name of Rectangle
-    def __init__(self, length: int, width: int): #__init__ it's a methodfor initialization
+class Rectangle: 
+    def __init__(self, length: int, width: int): 
         
-        self.dimensions = {'length': length, 'width': width} #It stores the length and width as a dictionary called dimensions
+        self.dimensions = {'length': length, 'width': width} 
         self.keys = list(self.dimensions.keys()) 
         self.index = 0
 
-    def __iter__(self): #it's a method to make the Class Iterable meaning that you can use it in a loop.
+    def __iter__(self): 
         return self
 
-    def __next__(self): # it's a method to Handle Iterations 
+    def __next__(self): 
         if self.index < len(self.keys):
-            #it returns a dictionary with the key and its corresponding value.
             key = self.keys[self.index]
             value = self.dimensions[key]
             self.index += 1 #increses by 1
             return {key: value}
-        raise StopIteration #If there are no more keys, it requests means raises StopIteration to stop the loop.
-
-    #It returns a string like Rectangle(length, width) when you call print(rect). This is useful for readability during debugging or displaying information about the object.
-    def __repr__(self): #it's a method for Displaying data in the way we want.
+        raise StopIteration 
+    
+    def __repr__(self): 
         return f"Rectangle({self.dimensions['length']}, {self.dimensions['width']})"
 
 rectangle_data = Rectangle(14, 31) 
